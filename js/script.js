17c2d22295dde94f2964dd9b6664cb7ee45cb153
Norman Ravasio <normanravasio@gmail.com>
2020-12-09 22:22:07 -0300

    comiteo scripts

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function Guardar() {
+
+    var nombre= document.getElementById("txtNombre") .value;
+    var nombre= document.getElementById("txtEdad") .value;
+    var nombre=prompt=i
+
+    if ( i<18)
+    alert("Sos menor")
+}
+    
